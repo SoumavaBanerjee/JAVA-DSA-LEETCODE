2a1d44b58b7ba23eb57ce998c03595d8356f98fe
Soumava Banerjee <soumava.rivu@gmail.com>
2022-03-13 00:10:20 +0530

    Time: 11 ms (47.34%), Space: 47.1 MB (43.70%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,45 +9,30 @@
  */
 class Solution {
     
-    public boolean getPath(TreeNode root, List<TreeNode> path, TreeNode target) {
-        
-        if(root == null)
-            return false;
-        
-        path.add(root);
-        
-         if (root.val == target.val)    
-            return true;
-        
-        if(getPath(root.left, path, target) || getPath(root.right, path, target))
-            return true;
-        
-        path.remove(path.size()-1);
-        return false;
-    }
     
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
         if(root == null)
             return null;
         
-        List<TreeNode> path1 = new ArrayList<TreeNode>();
-        List<TreeNode> path2 = new ArrayList<TreeNode>();
-        int itr;
         
-        getPath(root, path1, p);
-        getPath(root, path2, q);
+        if(root.val == p.val || root.val == q.val)
+            return root;
         
-        int size = Math.min(path1.size(), path2.size());
+        TreeNode pref = lowestCommonAncestor(root.left,p,q);
         
-        for(itr = 0; itr < size; itr++) 
-            if(path1.get(itr).val != path2.get(itr).val)
-                return path1.get(itr-1);
+        TreeNode qref = lowestCommonAncestor(root.right,p,q);
         
-            
+        if(pref != null && qref != null)
+            return root;
         
         
-        return path1.get(itr-1);
+        if(pref != null)
+            return pref;
         
+        if(qref != null)
+            return qref;
+        
+        return null;
     }
 }
