5a6d1cb3b2ef32ff81b59c745bfb07ca360f6b04
Soumava Banerjee <soumava.rivu@gmail.com>
2022-06-05 22:04:35 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (97.63%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,67 +1,24 @@
 public class Solution {
     public void setZeroes(int[][] matrix) {
-       
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        boolean firstRowHasZero = false;
-        boolean firstColHasZero = false;
-
-        for(int i=0; i<n; i++){
-            if(matrix[0][i]==0){
-                firstRowHasZero = true;
-                break;
-            }
-        }
-
-        for(int i=0; i<m; i++){
-            if(matrix[i][0]==0){
-                firstColHasZero = true;
-                break;
-            }
-        }
-
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        
+        int col = matrix[0].length;
+        int row = matrix.length;
+        
+        boolean[] isZeroPresentInRow = new boolean[row];
+        boolean[] isZeroPresentInCol = new boolean[col];
+        
+        for(int i = 0; i < row; i++)
+            for(int j = 0; j < col; j++)
+                if(matrix[i][j] == 0) {
+                    isZeroPresentInRow[i] = true;
+                    isZeroPresentInCol[j] = true;
                 }
-            }
-        }
-
-
-
-        for(int j=1;j<n; j++){
-            if(matrix[0][j]==0){
-                nullifyCol(matrix, j, m, n);
-            }
-        }
-
-        for(int i=1; i<m; i++){
-            if(matrix[i][0]==0){
-                nullifyRow(matrix, i, m, n);
-            }
-        }
-
-        if(firstRowHasZero){
-            nullifyRow(matrix, 0, m, n);
-        }
-        if(firstColHasZero){
-            nullifyCol(matrix, 0, m, n);
-        }
-
-    }
-
-    public void nullifyRow(int[][] matrix, int i, int m, int n){
-        for(int col=0; col<n; col++){
-            matrix[i][col] = 0;
-        }
-    }
-
-    public void nullifyCol(int[][] matrix, int j, int m, int n){
-        for(int row=0; row<m; row++){
-            matrix[row][j] = 0;
-        }
-    }
+                
+        
+        for(int i = 0; i < row; i++)
+            for(int j = 0; j < col; j++)
+                if( isZeroPresentInRow[i] || isZeroPresentInCol[j] )
+                    matrix[i][j] = 0;
+        
+    }     
 }
