16f0f69a655f6ec52116b7b1ca60a0a4a45cd1a5
Soumava Banerjee <soumava.rivu@gmail.com>
2022-06-11 22:26:35 +0530

    Time: 16 ms (27.62%), Space: 55.3 MB (43.39%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        
+        List<int[]> res = new ArrayList<>();
+        
+        if(intervals.length == 0)
+            return res.toArray(new int[0][]);
+        
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        
+        
+        for(int[] i : intervals) {
+            if(i[0] <= end) {
+                end = Math.max(i[1], end);
+            } else {
+                res.add(new int[]{start,end});
+                start = i[0];
+                end = i[1];    
+            }
+        }
+        
+        
+        res.add(new int[]{start,end});
+        return res.toArray(new int[0][]);
+        
+    }
+}
